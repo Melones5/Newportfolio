dfee7a3196593cbc69aa8156790117fde19e34f0
Melones5 <alexissantos1496@gmail.com>
2024-02-02 13:06:19 -0300

    changing some words

--- src/components/sections/About.jsx
+++ src/components/sections/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
       <h2 className='text-lg font-raleway font-bold text-primary-yellow'>Sobre mí</h2>
       <p className='pt-2 text-sm font-inter text-neutralText dark:text-darkText'>
         Mi nombre es <span className='font-bold'>Alexis</span>. </p>
-      <p className='pt-2 text-sm font-inter text-neutralText dark:text-darkText'>Estoy graduado de la carrera Analista de Sistemas y también soy técnico en Administracion.
+      <p className='pt-2 text-sm font-inter text-neutralText dark:text-darkText'>Estoy graduado de la carrera Analista de Sistemas y también soy técnico en Administración.
         Actualmente, me encuentro en búsqueda de mi primer experiencia laboral que me permita poner en práctica mis conocimientos y habilidades,
         así como seguir aprendiendo y creciendo profesionalmente. </p>
       <p className='pt-2 text-sm font-inter text-neutralText dark:text-darkText'>Mi objetivo es seguir capacitándome y adquiriendo conocimientos ya sea en el sector
